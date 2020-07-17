520cb944d6dfca3e074e3619de8163b574cc5aa9
Adam Peterka <peterkaa@gmatc.matc.edu>
2020-07-17 11:31:21 -0500

    added endpoint

--- server.js
+++ server.js
@@ -7,10 +7,26 @@ const app = express();
 //Connect dB
 connectDatabase();
 
+// configure middleware
+app.use(express.json({ extended: false }));
+
 //Api endpoints
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
 app.get("/", (req, res) =>
   res.send("http get request sent to root api endpoint")
 );
+
+/**
+ * @route POST api/users
+ * @desc Register User
+ */
+app.post("/api/users", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
 
 //Connection listener
 app.listen(3000, () => console.log("Express server running on port 3000"));
